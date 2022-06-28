affaa9881852df2a510c0d09e7ac4412afacbd91
Habib255 <arhabib255@gmail.com>
2022-06-29 00:35:37 +0600

    connect server with mongodb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
@@ -10,10 +11,31 @@ app.use(cors())
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ozrjf7g.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('moto_db').collection('products')
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
 app.get('/', (req, res) => {
-    res.send('hello wanna buy bike parts for your garage!')
+    res.send('do you want to buy bike parts for your garage!')
 })
 
 app.listen(port, () => {
-    console.log(`speedo parts listening on port ${port}`)
+    console.log(`moto parts listening on port ${port}`)
 })
