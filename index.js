e94c2a3accaac039a6a99ffd53e4a7de1a26da6c
Habib255 <arhabib255@gmail.com>
2022-07-27 06:11:22 +0600

    added userCollection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -19,19 +20,46 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db('moto_db').collection('products')
+        const orderCollection = client.db('moto_db').collection('orders')
+        const userCollection = client.db('moto_db').collection('users')
         app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await productCollection.findOne(query)
+            res.send(result)
+
+        })
+        app.post('/order', async (req, res) => {
+            const order = req.body
+            const result = await orderCollection.insertOne(order)
+            res.send(result);
+        })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateUser = {
+                $set: user,
+            }
+            const result = await userCollection.updateOne(filter, updateUser, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+        })
+
     }
     finally {
 
     }
 }
 run().catch(console.dir)
-
 app.get('/', (req, res) => {
     res.send('do you want to buy bike parts for your garage!')
 })
